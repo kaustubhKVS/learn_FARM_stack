d568480b7027f3d1a780b2ff430d8317ea58d68e
kaustubhKVS <kvshete15@gmail.com>
2023-01-10 12:17:27 -0800

    video 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 
 function App() {
+    const title = "Welcome to the blog"
+    const likes = 20;
+//  BOOLEAN or OBJECT cannot be returned
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked {likes} times </p>
       </div>
       
     </div>
