5f6689b089efce53eeca54ac556e35acce657943
kaustubhKVS <kvshete15@gmail.com>
2023-01-10 18:34:54 -0800

    navbar cover

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+const Home = () => {
+
+    const handleClick = () => {
+        console.log('hello, Ninjas')
+    }
+
+
+    return ( 
+
+        <div className="home">
+            <h1>WELCOME Ho</h1>
+            <button on >Click MEE</button>
+        </div>
+
+     );
+}
+ 
+export default Home;
+
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,18 @@
 const Navbar = () => {
     return ( 
+        
         <nav className="navbar">
         <h1>The Dojo Blog</h1>
         <div className="links">
             <a href="/">Home</a>
-            <a href="/create">New Blog</a>
+            
+            <a href="/create" style={{
+                color: 'white',
+                backgroundColor : '#f1356d',
+                borderRadius: '8px',
+            }
+                
+            }>New Blog</a>
         </div>
         </nav>
      );
