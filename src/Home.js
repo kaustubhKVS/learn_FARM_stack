faaddcf5581fd30e06a07d761984f9103ea3c3e4
kaustubhKVS <kvshete15@gmail.com>
2023-01-23 19:54:18 -0800

    video 8 and 9 hooks

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,13 @@
+import { useState } from "react";
+
 const Home = () => {
 
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
+
     const handleClick = (e) => {
+        setName('luigi');
+        setAge(30)
         console.log('hello, Ninjas', e)
     }
 
@@ -12,6 +19,7 @@ const handleClickAgain = (name, e) => {
 
         <div className="home">
             <h1>WELCOME Ho</h1>
+            <p>{name} is {age} years old</p>
             <button onClick={handleClick} >Click MEE</button>
             <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>
         </div>
@@ -24,7 +32,9 @@ export default Home;
 // handleClickAgain('mario')
 // We do not INVOKE by function handleClick(), we invoke by reference handleClick
 // Use Camel casing e.g. you just used OnClick insted of onClick, O capital nahi tha
-// Anonymous function ()=>{} , can support inline fucntion calls where instead of using curly braces, we can jsut write the code. 
-// we can use 'e' which is a browser property as an event log from browser.
-// Anonomous function can be used in an aonomous function to send and log browser data.
-  
+// Anonymous function ()=>{} , can support inline fucntion calls where instead of using curly braces, we can jsut write the code
+// we can use 'e' which is a browser property as an event log from browser
+// Anonomous function can be used in an aonomous function to send and log browser data
+
+// Reactive value is something which can change or re-render using state hooks
+// we use set states for this purpose, we decide a variable name and a mehtod which can be called to set the variable to a different value.
