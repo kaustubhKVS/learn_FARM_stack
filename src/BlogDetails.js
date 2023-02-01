1b7bd486c2195aca1f41dc10d5002011757c118f
kaustubhKVS <kvshete15@gmail.com>
2023-01-31 22:21:45 -0800

    using custom hooks and dynamic routing vid 26

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,13 +1,29 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
 
     const { blog_id } = useParams();
+    const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/'+blog_id);
+    const test = 'http://localhost:8000/blogs/'+blog_id;
 
     return ( 
         <div className="blog-details">
-            <h2>Blog Detials - { blog_id }</h2>
+            
+            {console.log({ test })}
+            {console.log({blog_id })}
 
+            { isPending && <div> LOADING CONTENT</div>}
+            
+            {error && <div> { error } </div> }
+            
+            {blog &&
+                <article>
+                    <h2>{ blog.title }</h2>
+                    <p> { blog.author }</p>
+                    <div> { blog.body }</div>
+                </article>
+            }
         </div>
      );
 }
